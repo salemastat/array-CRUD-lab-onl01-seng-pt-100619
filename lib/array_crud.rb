b585982001cd1b08c87debd3b2180bbab7b30313
Salem Asrat <salemasrat@gmail.com>
2019-10-12 17:54:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,11 +14,9 @@ def add_element_to_start_of_array(array, element)
   array.unshift(element)
 end	
 
-
 def remove_element_from_end_of_array(array)	
   array.pop
 end	
-
 
 def remove_element_from_start_of_array(array)	 
   array.shift
@@ -28,11 +26,9 @@ def retreive_element_from_index(array, index_number)
     array[index_number]
 end	
 
-
 def retreive_first_element_from_array(array)	 
   array[0]
 end	
-
 
 def retreive_last_element_from_array(array)	def 
   x = array.count
