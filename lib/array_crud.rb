ffd9f834d51659c10926bd9e7c49c41e22aca618
Salem Asrat <salemasrat@gmail.com>
2019-10-12 17:44:45 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,13 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-array = ["a", "b", "c", "d"]
+  array = ["a", "b", "c", "d"]
 end
 
-def add_element_to_end_of_array
-array = ["a", "b", "c", "d"]
-array << "e"
-end
+def add_element_to_end_of_array(array, element)
+  array << element
+end	end
 
 def add_element_to_start_of_array(array, element)
   
