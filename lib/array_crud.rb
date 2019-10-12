99a2ee6a2fafb5bc98421b1af0faeb00ca5507ca
Salem Asrat <salemasrat@gmail.com>
2019-10-12 17:50:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,19 +11,39 @@ def add_element_to_end_of_array(array, element)
 end	
 
 def add_element_to_start_of_array(array, element)
+
   array.unshift(element)
-end	
+end	end
 
-def remove_element_from_end_of_array(array)
-  
-end
 
-def remove_element_from_start_of_array(array)
-  
-end
+def remove_element_from_end_of_array(array)	def remove_element_from_end_of_array(array)
 
-def retrieve_element_from_index(array, index_number)
-  
+  array.pop
+end	end
+
+
+def remove_element_from_start_of_array(array)	def remove_element_from_start_of_array(array)
+
+  array.shift
+end	end
+
+
+def retreive_element_from_index(array, index_number)	def retreive_element_from_index(array, index_number)
+
+  array[index_number]
+end	end
+
+
+def retreive_first_element_from_array(array)	def retreive_first_element_from_array(array)
+
+  array[0]
+end	end
+
+
+def retreive_last_element_from_array(array)	def retreive_last_element_from_array(array)
+  x = array.count
+  array[x-1]
+end 	
 end
 
 def retrieve_first_element_from_array(array)
